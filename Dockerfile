8e1680f7235c0b093dfa3e0c55340eead8b384ab
Daniel <daniel@developerdan.com>
2021-12-15 02:18:54 +0000

    Added a working gravity based on master

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM pihole/pihole:latest
+FROM pihole/pihole:2021.11
 
+COPY gravity.sh /opt/pihole/gravity.sh
 COPY inject-lines.sh /inject-lines.sh
 RUN ./inject-lines.sh
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 
-docker-compose down && sudo rm -rf etc-pihole && docker-compose build --pull --no-cache && docker-compose up
+docker-compose down &&
+  sudo rm -rf etc-pihole etc-pihole-broken etc-pihole-fixed etc-pihole-test &&
+  docker-compose build --pull --no-cache &&
+  docker-compose up
